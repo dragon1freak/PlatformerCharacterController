dfcc1b6fde7f940211c377c4d35c1f19b3e2df37
James Ritchey <jim0ritchey@gmail.com>
2022-02-06 08:24:51 -0500

    Removed unneeded actions; broke up jumping and movement code for easier overrides.

--- PlatformerController.gd
+++ PlatformerController.gd
@@ -11,35 +11,29 @@ export(String) var ACTION_DOWN : String = "down"
 export(String) var ACTION_LEFT : String = "left"
 export(String) var ACTION_RIGHT : String = "right"
 export(String) var ACTION_JUMP : String = "jump"
-export(String) var ACTION_INTERACT : String = "interact"
-export(String) var ACTION_ALT_MOVE : String = "alt_move"
 
 export(bool) var JOYSTICK_MOVEMENT : bool = false
 
-export(float, 0, 1000, 0.1) var ACCELERATION : float = 300
-export(float, 0, 1000, 0.1) var MAX_SPEED : float = 50
-export(float, 0, 1000, 0.1) var FRICTION : float = 200
-export(float, 0, 1000, 0.1) var AIR_RESISTENCE : float
-export(float, 0, 1000, 0.1) var JUMP_FORCE : float
-export(float, 0, 1000, 0.1) var JUMP_CANCEL_FORCE : float
-export(float, 0, 1000, 0.1) var GRAVITY : float
-export(float, 0, 1, 0.01) var COYOTE_TIMER : float
-export(float, 0, 1, 0.01) var JUMP_BUFFER_TIMER : float
+export(float, 0, 1000, 0.1) var ACCELERATION : float = 500
+export(float, 0, 1000, 0.1) var MAX_SPEED : float = 100
+export(float, 0, 1000, 0.1) var FRICTION : float = 500
+export(float, 0, 1000, 0.1) var AIR_RESISTENCE : float = 200
+export(float, 0, 1000, 0.1) var JUMP_FORCE : float = 200
+export(float, 0, 1000, 0.1) var JUMP_CANCEL_FORCE : float = 800
+export(float, 0, 1000, 0.1) var GRAVITY : float = 500
+export(float, 0, 1, 0.01) var COYOTE_TIMER : float = 0.08
+export(float, 0, 1, 0.01) var JUMP_BUFFER_TIMER : float = 0.1
 
-enum {IDLE, WALK, JUMP, FALLING}
+enum {IDLE, WALK, JUMP, FALL}
 var state : int = IDLE
 var can_jump : bool = true
 var should_jump : bool = false
 var jumping : bool = false
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
 var motion : Vector2 = Vector2.ZERO
 func _physics_process(delta : float) -> void:
 	handle_inputs(delta)
+	manage_animations()
 	
 	if !is_on_floor() && can_jump:
 		coyote_time()
@@ -48,31 +42,51 @@ func _physics_process(delta : float) -> void:
 	motion.y += GRAVITY * delta
 	motion = move_and_slide(motion, Vector2.UP)
 
-func coyote_time() -> void:
-	yield(get_tree().create_timer(COYOTE_TIMER),"timeout")
-	can_jump = false
+func manage_state() -> void:
+	if motion.y == 0:
+		if motion.x == 0:
+			state = IDLE
+		else:
+			state = WALK
+	elif motion.y < 0:
+		state = JUMP
+	else:
+		state = FALL
 
-func buffer_jump() -> void:
-	should_jump = true
-	yield(get_tree().create_timer(JUMP_BUFFER_TIMER),"timeout")
-	should_jump = false
+func manage_animations() -> void:
+	if motion.x > 0:
+		_sprite.flip_h = false
+	elif motion.x != 0:
+		_sprite.flip_h = true
+	match state:
+		IDLE:
+			_animation_player.play("Idle")
+		WALK:
+			_animation_player.play("Walk")
+		JUMP:
+			_animation_player.play("Jump")
+		FALL:
+			_animation_player.play("Fall")
 
+# Gets the strength and status of the mapped actions and passes them to the handle_motion and handle_jump methods
+func handle_inputs(delta : float) -> void:
+	var input_direction : Vector2 = get_input_direction()
+	var jump_strength : float = Input.get_action_strength(ACTION_JUMP)
+	var jump_pushed : bool = Input.is_action_just_pressed(ACTION_JUMP)
+	var jump_released : bool = Input.is_action_just_released(ACTION_JUMP)
+	
+	handle_jump(jump_strength, jump_pushed, jump_released)
+	handle_motion(delta, input_direction)
+	manage_state()
+
+# Gets the X/Y axis movement direction using the input mappings assigned to the ACTION UP/DOWN/LEFT/RIGHT variables
 func get_input_direction() -> Vector2:
 	var x_dir = Input.get_action_strength(ACTION_RIGHT) - Input.get_action_strength(ACTION_LEFT)
 	var y_dir = Input.get_action_strength(ACTION_DOWN) - Input.get_action_strength(ACTION_UP)
 
 	return Vector2(x_dir if JOYSTICK_MOVEMENT else sign(x_dir), y_dir if JOYSTICK_MOVEMENT else sign(y_dir))
 
-func handle_inputs(delta : float) -> void:
-	var input_direction : Vector2 = get_input_direction()
-	var jump_strength : float = Input.get_action_strength(ACTION_JUMP)
-	var jump_pushed : bool = Input.is_action_just_pressed(ACTION_JUMP)
-	var jump_released : bool = Input.is_action_just_released(ACTION_JUMP)
-#	var interact_strength : float = Input.get_action_strength(ACTION_INTERACT)
-#	var alt_move_strength : float = Input.get_action_strength(ACTION_ALT_MOVE)
-	handle_jump(jump_strength, jump_pushed, jump_released)
-	handle_motion(delta, input_direction)
-
+# ------------------ Movement Logic ---------------------------------
 func handle_motion(delta : float, input_direction : Vector2 = Vector2.ZERO) -> void:
 	if input_direction.x != 0:
 		apply_motion(input_direction, delta)
@@ -87,29 +101,38 @@ func apply_motion(move_direction : Vector2, delta : float) -> void:
 
 func apply_friction(delta : float) -> void:
 	var fric = FRICTION * delta * sign(motion.x) * -1 if is_on_floor() else AIR_RESISTENCE * delta * sign(motion.x) * -1
-	motion.x += fric
-	if abs(motion.x) < fric:
+	if abs(motion.x) <= abs(fric):
 		motion.x = 0
+	else:
+		motion.x += fric
 
+
+# ------------------ Jumping Logic ---------------------------------
 func handle_jump(jump_strength : float = 0.0, jump_pressed : bool = false, jump_released : bool = false) -> void:
 	if (jump_pressed or should_jump) && can_jump:
 		apply_jump()
 	elif jump_pressed:
 		buffer_jump()
-	elif jump_released:
-		if motion.y < 0:
-			jumping = false
+	elif jump_strength == 0 and jumping:
+		jumping = false
 	if is_on_floor():
 		can_jump = true
-		if should_jump:
-			apply_jump()
-			should_jump = false
 
 enum JUMP_DIRECTIONS {UP = -1, DOWN}
 func apply_jump(jump_force : float = JUMP_FORCE, jump_direction : int = JUMP_DIRECTIONS.UP) -> void:
 	can_jump = false
+	should_jump = false
 	jumping = true
 	motion.y += jump_force * jump_direction
 
-func cancel_jump(delta: float, jump_direction : int = JUMP_DIRECTIONS.UP) -> void:
+func cancel_jump(delta : float, jump_direction : int = JUMP_DIRECTIONS.UP) -> void:
 	motion.y -= JUMP_CANCEL_FORCE * jump_direction * delta
+
+func buffer_jump() -> void:
+	should_jump = true
+	yield(get_tree().create_timer(JUMP_BUFFER_TIMER),"timeout")
+	should_jump = false
+
+func coyote_time() -> void:
+	yield(get_tree().create_timer(COYOTE_TIMER),"timeout")
+	can_jump = false
